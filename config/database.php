104d5a535cbf01772abe8ff032ef7e9e78c78d3c
gichi939 <gichi939@gmail.com>
2021-05-08 19:28:59 +0900

    デプロイ自動化テスト15

--- config/database.php
+++ config/database.php
@@ -35,6 +35,20 @@ return [
 
     'connections' => [
 
+        'protan' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'protan',
+            'username' => 'root',
+            'password' => 'secret',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
