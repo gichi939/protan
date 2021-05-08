590218af9150d5f482cbc43f387663cf3ca05cb4
gichi939 <gichi939@gmail.com>
2021-05-08 18:14:54 +0900

    デプロイ自動化テスト5

--- config/database.php
+++ config/database.php
@@ -39,7 +39,7 @@ return [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
             'port' => '3306',
-            'database' => 'circle_test',
+            'database' => 'protan',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8mb4',
