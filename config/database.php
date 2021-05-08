b846f6ee87fe744235a5b4cc43b36b857a531bdf
gichi939 <gichi939@gmail.com>
2021-05-08 21:08:00 +0900

    デプロイ20

--- config/database.php
+++ config/database.php
@@ -35,11 +35,11 @@ return [
 
     'connections' => [
 
-        'protan' => [
+        'circle_test' => [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
             'port' => '3306',
-            'database' => 'protan',
+            'database' => 'circle_test',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8mb4',
