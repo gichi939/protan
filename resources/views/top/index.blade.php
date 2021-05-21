f5f9b71fe133bbf22498265f0846eda65a73c29b
gichi939 <gichi939@gmail.com>
2021-05-21 12:27:04 +0900

    【TopPage】コースのパネルcss変更

--- resources/views/top/index.blade.php
+++ resources/views/top/index.blade.php
@@ -8,19 +8,29 @@
     <div class="language-course">
         <div class="row">
             <div class="course-item col-5">
-            <h1 class="course-title">HTML編</h1>
-                <img class="course-image" src=" {{ asset('image/HTML5_Logo.png') }}" alt="">
-                <div class="word-number">
-                    〇〇単語
-                </div>
+                <a class="cource-link" href="{{ route('html.show') }}">
+                    <div>
+                        <h1 class="course-title">HTML編</h1>
+                        <img class="course-image" src=" {{ asset('image/HTML5_Logo.png') }}" alt="">
+                        <div class="word-number">
+                            〇〇単語
+                        </div>
+                    </div>
+                </a>
             </div>
             <div class="offset-1"></div>
             <div class="course-item col-5">
-            <h1 class="course-title">CSS編</h1>
-                <img class="course-image" src=" {{ asset('image/css3_logo.png') }}" alt="">
-                <div class="word-number">
-                    〇〇単語
-                </div>
+                <a class="cource-link" href="">
+                    <div>
+                        <h1 class="course-title">CSS編</h1>
+                        <img class="course-image" src=" {{ asset('image/css3_logo.png') }}" alt="">
+                        <div class="word-number">
+                            <p>
+                                〇〇単語
+                            </p>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
